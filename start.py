69d55303b30e352a2728b45091c9bef2bc099a2d
Highlander Dantas <haylander60@gmail.com>
2019-05-13 14:30:21 -0300

    save.

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
     resource = boto3.resource('ec2')
     client = boto3.client('ec2', region_name=region)
 
-    for instance in resource.instances.filter(Filters=[{'Name': 'tag:Shutdown', 'Values': ['True']}]):
+    for instance in resource.instances.filter(Filters=[{'Name': 'tag:Shutdown', 'Values': ['True', 'true']}]):
         resultado = instance.id
         client.start_instances(InstanceIds=[str(resultado)])
         print('Start Instance: ' + str(resultado))
